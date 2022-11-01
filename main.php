715366ad6f68a34c65c5b9d68bd414dd7102a62c
TheMadMystic <65517955+TheMadMystic@users.noreply.github.com>
2022-11-01 15:26:32 -0700

    Navigation bar made
    Created a navigation bar php file that is used across all pages

--- main.php
+++ main.php
@@ -1,13 +1,16 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8"/>
     <title>MergeLit Main Page</title>
     <link rel="stylesheet" href="theStyles.css"/>
 </head>
 <body>
-<h1>MergeLit</h1>
-<p>Here is where we'll put the file drag and drop zone and the submit button</p>
+<?php
+    include 'navBar.php';
+?>
+<h1>Merge Page</h1>
+<div class="placeholder">Here is where we'll put the file drag and drop zone and the submit button</div>
 </body>
 </html>
 
--- memberships.php
+++ memberships.php
@@ -1,12 +1,15 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8"/>
     <title>MergeLit Membership Page</title>
     <link rel="stylesheet" href="theStyles.css"/>
 </head>
 <body>
+<?php
+include 'navBar.php';
+?>
 <h1>Memberships</h1>
-<p>Here is where we'll put the membership options</p>
+<div class="placeholder">Here is where we'll put the membership options</div>
 </body>
 </html>

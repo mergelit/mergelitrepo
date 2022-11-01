479af88521f8fdf63eea030444f8737e40d3a32d
TheMadMystic <65517955+TheMadMystic@users.noreply.github.com>
2022-11-01 13:51:53 -0700

    main pages and css page
    added simple pages for main page, membership page, and css page

---
+++ main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>MergeLit Main Page</title>
+    <link rel="stylesheet" href="theStyles.css"/>
+</head>
+<body>
+<h1>MergeLit</h1>
+<p>Here is where we'll put the file drag and drop zone and the submit button</p>
+</body>
+</html>
+
---
+++ memberships.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>MergeLit Membership Page</title>
+    <link rel="stylesheet" href="theStyles.css"/>
+</head>
+<body>
+<h1>Memberships</h1>
+<p>Here is where we'll put the membership options</p>
+</body>
+</html>

82bfe480006979effde08e0c7def027b5ade48a7
mlorhan <mlorhan@ewu.edu>
2022-11-07 19:41:48 -0800

    Creating Account Details page
    using css page, navBar, has list items of account information we may track, gray rectangles represent where the information will be presented

---
+++ accountDetails.php
@@ -0,0 +1,45 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>MergeLit Account Details Page</title>
+    <link rel="stylesheet" href="theStyles.css"/>
+</head>
+<body>
+<?php
+include 'navBar.php';
+?>
+<h1>Account Details</h1>
+<div class="placeholder">Here is where we'll put the account details
+    <div id = "detailList" style = "text-align: left;">
+        <ul>
+            <li>Account Name: 
+                <svg width="400" height="15">
+                    <rect width="400" height="15" style="fill:rgb(80,80,80);" />
+                </svg>
+            </li>
+            <li>Email: 
+                <svg width="400" height="15">
+                    <rect width="400" height="15" style="fill:rgb(80,80,80);" />
+                </svg>    
+            </li>
+            <li>Password: 
+                <svg width="400" height="15">
+                    <rect width="400" height="15" style="fill:rgb(80,80,80);" />
+                </svg>
+            </li>
+            <li>Social Security Number: 
+                <svg width="400" height="15">
+                    <rect width="400" height="15" style="fill:rgb(80,80,80);" />
+                </svg>
+            </li>
+            <li>Account Identifier: 
+                <svg width="400" height="15">
+                    <rect width="400" height="15" style="fill:rgb(80,80,80);" />
+                </svg>
+            </li>
+        </ul>
+    </div>
+</div>
+</body>
+</html>

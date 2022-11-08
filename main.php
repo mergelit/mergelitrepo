312fdfb599b5b28cb9e2132605c12834ca20645f
mlorhan <mlorhan@ewu.edu>
2022-11-07 19:14:36 -0800

    Adding Merge button

--- main.php
+++ main.php
@@ -18,10 +18,14 @@
 <h1>Merge PDFs</h1>
 
 <div class="placeholder">
-    <div id="Fire emoji">
+    <div id="fireEmoji">
         <img src = "images\fire_1f525.png" alt = "Epic fire emoji">
     </div>
     <button type = "button">Select Files</button>
+</div>
+
+<div id = "mergeButton">
+    <button type = "button">MERGE</button>
 </div>
 
 

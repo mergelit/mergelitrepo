a41364f57e8e0c8ffa38f304fa12f4eb250f2df6
Clockknight <hello@tylerhanyinwang.com>
2022-11-07 23:24:27 -0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>Log Into MergeLit</title>
+    <link rel="stylesheet" href="theStyles.css"/>
+</head>
+<body>
+<?php
+
+?>
+<h1>Merge Page</h1>
+<div class="placeholder">Here is where we'll put the file drag and drop zone and the submit button</div>
+</body>
+</html>

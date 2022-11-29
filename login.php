0f2c10e0561d5be4536229c8bc69f9da13902a58
mlorhan <mlorhan@ewu.edu>
2022-11-29 10:02:20 -0800

    Changing DB connection from MYSQLi to PDO

--- login.php
+++ login.php
@@ -28,23 +28,26 @@
 <?php
     include 'navBar.php';
 
+    /*
     //Need to create a database still
     //If the connection is opened here does it stay open while on the website or close if you switch pages?
-    /*
+
     //Opening DB connection, variables to be replaced
     $servername = "localhost";
     $username = "username";
     $password = "password";
 
-    //Create connection
-    $conn = new mysqli($servername, $username, $password);
-
-    //Check connection
-    if($conn->connect_error){
-        die("Connection failed: ".$conn->connect_error);
+    try{
+        //Create connection
+        $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+        //Set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERROMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
+    } catch(PDOException $e) {
+        echo "Connection failed: ".$e->getMessage();
     }
-    echo "Connected successfully";
     */
+
 ?>
 <h1>Log In Here:</h1>
 <div class="container">

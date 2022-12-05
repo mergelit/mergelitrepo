e0c361f78c33fed5de2892a0cf62ea37b4c107b5
Clockknight <hello@tylerhanyinwang.com>
2022-12-04 16:14:38 -0800

    junk code to try and get pdf functions working

---
+++ merge.js
@@ -0,0 +1,23 @@
+export async function merge() {
+    import { PDFDocument } from "pdf-lib"
+
+    console.log("ahhahahaha") //This only works if the compiler doesnt get an error with the above import statement
+
+    /*const file1 = document.getElementById('file-1').value
+    const file2 = document.getElementById('file-2').value
+    const mergedPdf = await PDFDocument.create()
+
+    console.log("mergePDFs")
+
+    const pdfA = await PDFDocument.load(fs.readFileSync(file1))
+    const pdfB = await PDFDocument.load(fs.readFileSync(file2))
+
+    const copiedPagesA = await mergedPdf.copyPages(pdfA, pdfA.getPageIndices())
+    copiedPagesA.forEach((page) => mergedPdf.addPage(page))
+    const copiedPagesB = await mergedPdf.copyPages(pdfB, pdfB.getPageIndices())
+    copiedPagesB.forEach((page) => mergedPdf.addPage(page))
+
+    const mergedPdfFile = await mergedPdf.save()
+
+    console.log(mergedPdfFile)*/
+}
--- scripttestpage.php
+++ scripttestpage.php
@@ -54,6 +54,9 @@
         }
 
     </style>
+
+
+
 </head>
 <body>
 <?php
@@ -79,43 +82,28 @@ include 'navBar.php';
     </div>
 
     <br>
-    <script>
-        function test() {
-            const file1 = document.getElementById('file-1').value;
-            const file2 = document.getElementById('file-2').value;
-            console.log(pdfsToMerge);
-        }
+    <button type=button id="merge">MERGE</button>
+    <script type="module">
+        import {merge} from './merge.js'
+
+        document.querySelector('#merge').addEventListener('click', merge);
     </script>
-    <button id="mergeButton" style= >MERGE</button>
+
+    <!-- <button type=button id="test">test</button>
+    <script type="module">
+        import {test} from './test.js'
+
+        document.querySelector('#test').addEventListener('click', test);
+    </script>
+    <button type="button" id="hello">Click Me</button>
+    <script type="module">
+        import {hello} from './hello.js'
+
+        document.querySelector('#hello').addEventListener('click', hello)
+    </script> -->
 </div>
 
 
-
-
-<script type="module" src='https://cdn.jsdelivr.net/npm/pdf-lib/dist/pdf-lib.js'>
-
-    document.getElementById("mergeButton").onclick=async ()=>{
-
-    const file1 = document.getElementById('file-1').value;
-    const file2 = document.getElementById('file-2').value;
-
-    const mergedPdf = await PDFDocument.create();
-
-    const pdfA = await PDFDocument.load(fs.readFileSync(file1));
-    const pdfB = await PDFDocument.load(fs.readFileSync(file2));
-
-    const copiedPagesA = await mergedPdf.copyPages(pdfA, pdfA.getPageIndices());
-    copiedPagesA.forEach((page) => mergedPdf.addPage(page));
-
-    const copiedPagesB = await mergedPdf.copyPages(pdfB, pdfB.getPageIndices());
-    copiedPagesB.forEach((page) => mergedPdf.addPage(page));
-
-    const mergedPdfFile = await mergedPdf.save();
-
-    console.log(mergedPdfFile);
-
-    };
-</script>
 </body>
 </html>
 
